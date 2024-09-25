4daf219a34b2abd2b523551f1a8f868bc8c9b784
Sahyan <tahirihasinarakotmanga@gmail.com>
2024-09-25 08:14:08 +0300

    page d'accueil

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Social Valyh</title>
+</head>
+<body>
+    <h1>Social Valyh</h1>
+    <a href="login.php">Se Connecter</a>
+    <a href="register.php">S'inscrire</a>
+</body>
+</html>
