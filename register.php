ec921fcc9667f649016eb4d53ea0ea1a64986cd8
Sahyan <tahirihasinarakotmanga@gmail.com>
2024-09-25 12:00:04 +0300

    register

--- register.php
+++ register.php
@@ -8,6 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt = $pdo->prepare('INSERT INTO users (email, password) VALUES (?, ?)');
     if ($stmt->execute([$email, $password])) {
         echo 'Inscription réussie ! <a href="login.php">Connectez-vous</a>';
+        header("Location : login.php");
     } else {
         echo 'Erreur lors de l\'inscription.';
     }
@@ -19,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>INscription</title>
+    <title>Inscription</title>
 </head>
 <body>
     <form method="POST">
