831ebdd3cf8963047cd15838281eb2c988bd8aa0
Sahyan <tahirihasinarakotmanga@gmail.com>
2024-09-26 11:54:07 +0300

    utilisation d'un seul fichier css

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styleIndex.css">
+    <link rel="stylesheet" href="style.css">
     <title>Social Valyh</title>
 </head>
-<body>
-    <h1>Social Valyh</h1>
-    <a href="login.php">Se Connecter</a>
-    <a href="register.php">S'inscrire</a>
+<body class="body_index">
+    <h1 class="h1_index">Social Valyh</h1>
+    <a class="a_index" href="login.php">Se Connecter</a>
+    <a class="a_index" href="register.php">S'inscrire</a>
 </body>
 </html>
