2975c0e87f8dd0e5c6de4e977dacae58f14d114d
Sahyan <tahirihasinarakotmanga@gmail.com>
2024-10-02 16:02:49 +0300

    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Social Valyh</title>
+</head>
+<body class="body-index">
+    <h1 class="h1-index">Social Valyh</h1>
+    <a class="a-index" href="./src/login.php">Se Connecter</a>
+    <a class="a-index" href="./src/register.php">S'inscrire</a>
+</body>
+</html>
