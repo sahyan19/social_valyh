6c2853fea68d0a821724d6176c73f655cc9af2b9
Sahyan <tahirihasinarakotmanga@gmail.com>
2024-10-02 15:54:38 +0300

    gestion des class dans les pages

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <link rel="stylesheet" href="style.css">
     <title>Social Valyh</title>
 </head>
-<body class="body_index">
-    <h1 class="h1_index">Social Valyh</h1>
-    <a class="a_index" href="login.php">Se Connecter</a>
-    <a class="a_index" href="register.php">S'inscrire</a>
+<body class="body-index">
+    <h1 class="h1-index">Social Valyh</h1>
+    <a class="a-index" href="login.php">Se Connecter</a>
+    <a class="a-index" href="register.php">S'inscrire</a>
 </body>
 </html>
